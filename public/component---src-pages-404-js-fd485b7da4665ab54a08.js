c28b3bd2108f73353377cb57aaf335bc4cb7a3be
MarceliBil <marbil003@gmail..com>
2021-07-21 03:43:52 +0200

    gatsby build fix

---
+++ public/component---src-pages-404-js-fd485b7da4665ab54a08.js
@@ -0,0 +1,2 @@
+(self.webpackChunkjohn_kowalsky=self.webpackChunkjohn_kowalsky||[]).push([[883],{9616:function(e,t,n){"use strict";n.r(t);var l=n(7294),a=n(5444);t.default=function(){return l.createElement("main",null,l.createElement("title",null,"Not found"),l.createElement("h1",null,"Site doesn't exist"),l.createElement("br",null),l.createElement(a.Link,{to:"/"},l.createElement("div",{className:"btn"},l.createElement("button",null,"Go home"))))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-fd485b7da4665ab54a08.js.map
